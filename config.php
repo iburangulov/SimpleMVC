5b15a4f1e2c9a3ea4865237cd27725b9a75ec5d3
iburangulov <iburanguloff@ya.ru>
2020-12-21 06:33:47 -0600

    Base views added

--- config.php
+++ config.php
@@ -6,6 +6,8 @@ define('ROOT', dirname(__FILE__) . '/');
 define('ROUTES', ROOT . 'routes/web.php');
 
 define('CONTROLLERS_PATH', ROOT . 'controllers/');
+define('MODELS_PATH', ROOT . 'models/');
+define('VIEWS_PATH', ROOT . 'views/');
 
 define('DB_NAME', 'test.loc');
 define('DB_HOST', 'localhost');
---
+++ views/posts/single.php
@@ -0,0 +1,14 @@
+<?php
+include_once VIEWS_PATH . 'templates/header.php';
+?>
+<div class="content">
+
+
+
+<h1>Post <?php echo $id; ?></h1>
+
+
+
+</div>
+<?php
+include_once VIEWS_PATH . 'templates/footer.php';
